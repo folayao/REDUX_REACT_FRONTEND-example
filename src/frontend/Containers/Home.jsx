9008675790635a9eb43bc76a2763bde2c5c0b9e5
Felipe Olaya Ospina <folayao@eafit.edu.co>
2021-01-08 14:20:19 -0500

    Here we go, webpack still throwing a error. :D

--- src/frontend/Containers/Home.jsx
+++ src/frontend/Containers/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import button from 'bootstrap'
+import Button from '@material-ui/core/Button';
 class Home extends Component {
     render() {
         return (
